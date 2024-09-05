2e7ac500a8f4a0674b95f77d190f2014008ecac6
Paolo Gomez <peweegardner248@gmail.com>
2024-09-05 11:28:10 -0700

    update

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ int main() {
         "A2,Suzan,Erickson,Erickson_1990@gmailcom,19,50,30,40,NETWORK",
         "A3,Jack,Napoli,The_lawyer99yahoo.com,19,20,40,33,SOFTWARE",
         "A4,Erin,Black,Erin.black@comcast.net,22,50,58,40,SECURITY",
-        "A5,[firstname],[lastname],[emailaddress],[age],[daysInCourse1],[daysInCourse2],[daysInCourse3],SOFTWARE"
+        "A5,[Paolo],[Gomez],[pgome81@wgu.edu],[28],[daysInCourse1],[daysInCourse2],[daysInCourse3],SOFTWARE"
     };
 
     Roster classRoster;
