5fe00c74b466f928de378787fe2a0c8758dd4055
Paolo Gomez <peweegardner248@gmail.com>
2024-09-05 12:01:48 -0700

    update

--- main.cpp
+++ main.cpp
@@ -5,15 +5,15 @@ int main() {
     // Print out course title, programming language, WGU student ID, and name
     std::cout << "Course Title: Scripting and Programming - Applications - C867" << std::endl;
     std::cout << "Programming Language: C++" << std::endl;
-    std::cout << "WGU Student ID: [Your WGU Student ID]" << std::endl;
-    std::cout << "Name: [Paolo Gomez]" << std::endl << std::endl;
+    std::cout << "WGU Student ID: 012249526" << std::endl;
+    std::cout << "Name: Paolo Gomez" << std::endl << std::endl;
 
     const std::string studentData[] = {
         "A1,John,Smith,John1989@gm ail.com,20,30,35,40,SECURITY",
         "A2,Suzan,Erickson,Erickson_1990@gmailcom,19,50,30,40,NETWORK",
         "A3,Jack,Napoli,The_lawyer99yahoo.com,19,20,40,33,SOFTWARE",
         "A4,Erin,Black,Erin.black@comcast.net,22,50,58,40,SECURITY",
-        "A5,[Paolo],[Gomez],[pgome81@wgu.edu],[28],[daysInCourse1],[daysInCourse2],[daysInCourse3],SOFTWARE"
+        "A5,Paolo,Gomez,pgome81@wgu.edu,28,35,40,45,SOFTWARE"
     };
 
     Roster classRoster;
